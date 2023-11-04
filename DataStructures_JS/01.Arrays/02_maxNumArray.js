7cec403eb27b726c18d507e0d0716a6a9c5d8dad
md musharruf <mdmusharruf13@gmail.com>
2023-11-04 23:42:56 +0530

    max number in array

---
+++ DataStructures_JS/01.Arrays/02_maxNumArray.js
@@ -0,0 +1,11 @@
+function maxNumber(arr) {
+    return Math.max(...arr);
+}
+
+let array = [11,5,2,10];
+
+console.log(maxNumber(array));
+
+console.log(Math.max([2,5,1,9]));
+
+console.log(Math.max(1,5,9,2));
