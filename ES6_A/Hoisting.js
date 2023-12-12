290bd9b0b79ec97eeb4e8f7c09529bfdb7f50222
md musharruf <mdmusharruf13@gmail.com>
2023-12-12 21:14:15 +0530

    hoisting

---
+++ ES6_A/Hoisting.js
@@ -0,0 +1,26 @@
+getName();
+console.log(a);
+
+var a = 5;
+function getName() {
+    console.log("JavaScript is here")
+}
+
+
+// wish();
+
+// let wish = function() {
+//     console.log("hi js");
+// }
+
+
+var wish1 = function() {
+    console.log("hi js");
+}
+
+wish1();
+
+
+// console.log(b);
+
+// let b = 20;
