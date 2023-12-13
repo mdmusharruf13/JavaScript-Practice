6eb0bb6d758f99764b35ffcdbcd57ada69f662d1
md musharruf <mdmusharruf13@gmail.com>
2023-12-13 18:38:00 +0530

    promises in js

---
+++ ES6_A/promise.js
@@ -0,0 +1,34 @@
+const array = [
+    { name: "musharruf", age: 29 },
+    { name: "nawaz", age: 25 },
+];
+
+function printDetails() {
+    setTimeout(() => {
+        let list = "";
+        array.forEach((item) => {
+            list += `<li>${item.name}</li>`;
+        });
+        document.body.innerHTML = list;
+    },1000);
+}
+
+function createData(newData) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            array.push(newData);
+            let error = true;
+            if(error) {
+                resolve();
+            }
+            else {
+                reject("something went wrong!!");
+            }
+    
+        },2000);
+    })
+}
+
+createData({name: "musharruf nawaz", age: 35})
+    .then(printDetails)
+    .catch(err => console.log(err));
