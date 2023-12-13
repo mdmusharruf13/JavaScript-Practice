7e07293a9f5c27cba4be33b864322e003ad9669f
md musharruf <mdmusharruf13@gmail.com>
2023-12-13 18:34:53 +0530

    sort higher order function

---
+++ ES6_A/sort.js
@@ -0,0 +1,12 @@
+const ages = [33,10,51,52,89,23,45,65,80,75,41,22,35];
+
+console.log(ages);
+
+ages.sort((a,b) => a-b);
+
+console.log(ages);
+
+
+const nums = ["ab","a","cccc","b"].sort();
+
+console.log(nums);
